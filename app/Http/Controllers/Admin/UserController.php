a0ad51fdcff68f3a3af3cd5ac3a61446dd613533
Gustavo Paulino <cast.gustavo.utic@sebrae.com.br>
2025-05-24 21:22:37 -0300

    Lista milhares de usuários (paginação)

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = User::first();
-        return view('admin.users.index', compact('user'));
+        $users = User::paginate(20);//User::all();
+        return view('admin.users.index', compact('users'));
     }
 }
