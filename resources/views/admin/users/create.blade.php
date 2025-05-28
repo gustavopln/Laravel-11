ca8f8d37949327dd5afd6585b5b6da5d3caffc9c
Gustavo Paulino <cast.gustavo.utic@sebrae.com.br>
2025-05-28 00:06:56 -0300

    Templates com o blade

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,10 +1,18 @@
-<h1>Novo Usuário</h1>
+@extends('admin.layouts.app')
 
-<form action="{{ route('users.store') }}" method="POST">
-    <!-- <input type="text" name="_token" value="{{ csrf_token() }}"> /*Uma forma mais elegante é utilizando apenas @csrf(), que irá criar o um campo hidden*/-->
-     @csrf()
-    <input type="text" name="name" placeholder="Nome">
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
-</form>
+@section('title','Cadastro de Usuários')
+
+@section('content')
+
+    <h1>Novo Usuário</h1>
+
+    <form action="{{ route('users.store') }}" method="POST">
+        <!-- <input type="text" name="_token" value="{{ csrf_token() }}"> /*Uma forma mais elegante é utilizando apenas @csrf(), que irá criar o um campo hidden*/-->
+        @csrf()
+        <input type="text" name="name" placeholder="Nome">
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="password" name="password" placeholder="Senha">
+        <button type="submit">Enviar</button>
+    </form>
+
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('admin.layouts.app')
+
+@section('title','Listagem de Usuários')
+
+@section('content')
     <h1>Usuários</h1>
 
     <a href="{{ route('users.create') }}">Novo Usuário</a>
@@ -34,6 +31,4 @@
     </table>
 
     {{ $users->links() }}
-
-</body>
-</html>
+@endsection
