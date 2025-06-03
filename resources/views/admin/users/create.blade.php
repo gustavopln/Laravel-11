c301bbee0a1541891b99d561c6c037f459826dff
Gustavo Paulino <cast.gustavo.utic@sebrae.com.br>
2025-06-03 01:59:01 -0300

    Criando view reutilizável

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,19 +3,11 @@
 @section('title','Cadastro de Usuários')
 
 @section('content')
-
     <h1>Novo Usuário</h1>
-
     {{-- @include('admin.includes.errors') --}}
-    <x-alert/>
-
     <form action="{{ route('users.store') }}" method="POST">
-        <!-- <input type="text" name="_token" value="{{ csrf_token() }}"> /*Uma forma mais elegante é utilizando apenas @csrf(), que irá criar o um campo hidden*/-->
-        @csrf()
-        <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
-        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
-        <input type="password" name="password" placeholder="Senha">
-        <button type="submit">Enviar</button>
+        <!-- <input type="text" name="_token" value="{{ csrf_token() }}"> /*Uma forma mais elegante é utilizando apenas @csrf(), que irá criar o um campo hidden*/-->        
+        @include('admin/users/partials.form')
     </form>
 
 @endsection
