82c9684022f4fe109894c79f22c895c208a79b5f
Gustavo Paulino <cast.gustavo.utic@sebrae.com.br>
2025-06-03 02:35:08 -0300

    Adicionado visualização e exclusão do usuário

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -57,5 +58,29 @@ class UserController extends Controller
         return redirect()
             ->route('users.index')
             ->with('message', 'Usuário editado com sucesso');
+    }
+
+    public function show(string $id)
+    {
+        if (!$user = User::find($id)){
+            return redirect()->route('users.index')->with('message', 'Usuário não encontra');
+        }
+        
+        return view('admin.users.show', compact('user'));
+    }
+
+    public function destroy(string $id)
+    {
+        if (!$user = User::find($id)){
+            return redirect()->route('users.index')->with('message', 'Usuário não encontra');
+        }
+        if (Auth::user()->id === $user->id){
+            return back()->with('message', 'Você não pode deletar seu próprio perfil');
+        }
+        $user->delete();
+
+        return redirect()
+            ->route('users.index')
+            ->with('success', 'Usuário excluído com sucesso');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::delete('/users/{user}/destroy', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::put('/user/{user}', [UserController::class, 'update'])->name('users.update');
 Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
